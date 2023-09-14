a65ec9022f056b0ca4fada0566d8ef9ef9cfaacd
Benoît Delrue <33382345+BenoitDlr@users.noreply.github.com>
2023-09-14 15:15:32 +0200

    Update index.js with secured https path

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from "./pages";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://catstronauts-server-production-855e.up.railway.app/", // change to YOUR own production server
+  uri: "https://catstronauts-server-production-855e.up.railway.app/", // change to YOUR own production server
   cache: new InMemoryCache(),
   name: "web",
   version: "1.0",
